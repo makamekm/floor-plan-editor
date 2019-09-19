d00920e5bbb9a0b6ec545d7d919d9de3abbfcf43
Максим Карпов <makame@mail.ru>
2019-09-20 00:17:45 +0200

    Editor view

--- components/blueprint.jsx
+++ components/blueprint.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Blueprint } from '../models/blueprint';
 import ToggleButtonType from './toggle-type';
+import Panel from './panel';
 
 const itemTypeList = [
   {
@@ -57,6 +58,60 @@ class BlueprintView extends Component {
             onToggle={key => this.changeMode(key)}
           />
         </div>
+
+        <div className="items-panel">
+          <Panel>
+            <div className="list">
+              <div className="item clickable">
+                Add Table
+              </div>
+              <div className="item clickable">
+                Add Computer
+              </div>
+              <div className="item clickable">
+                Add Warning
+              </div>
+            </div>
+          </Panel>
+        </div>
+
+        <div className="property-panel">
+          <Panel>
+            <div className="list">
+              <div className="item header">
+                Table
+              </div>
+              <div className="item">
+                Owner: Karpov Maxim
+              </div>
+              <div className="item">
+                Date added: 11/12/2019
+              </div>
+              <div className="item header">
+                Description
+              </div>
+              <div className="item">
+                The best table has ever made
+              </div>
+            </div>
+          </Panel>
+        </div>
+
+        <div className="floor-panel">
+          <Panel>
+            <ToggleButtonType
+              activeState={'2 floor'}
+              items={[{
+                key: '2 floor',
+                name: '2 floor',
+              }, {
+                key: 'menu',
+                name: '=',
+              }]}
+              onToggle={key => {}}
+            />
+          </Panel>
+        </div>
   
         <style jsx>{`
           .view {
@@ -70,6 +125,83 @@ class BlueprintView extends Component {
             bottom: 20px;
             left: 50%;
             transform: translateX(-50%);
+          }
+
+          .items-panel {
+            position: absolute;
+            top: 20px;
+            right: 20px;
+            border-radius: 5px;
+            width: 300px;
+          }
+
+          .property-panel {
+            position: absolute;
+            top: 20px;
+            left: 20px;
+            border-radius: 5px;
+            width: 300px;
+          }
+
+          .floor-panel {
+            position: absolute;
+            top: 20px;
+            left: 50%;
+            transform: translateX(-50%);
+          }
+
+          .list {
+            overflow: auto;
+            max-height: calc(100vh - 20px);
+          }
+
+          .item {
+            padding-left: 20px;
+            padding-right: 20px;
+            transition: background-color 0.1s;
+            will-change: background-color;
+            user-select: none;
+            padding-top: 10px;
+            padding-bottom: 10px;
+            font-family: Open Sans;
+            font-style: normal;
+            font-size: 12px;
+            line-height: 12px;
+            border-bottom: 1px solid #f1f1f1;
+          }
+
+          .item:last-child {
+            border-bottom: none;
+          }
+
+          .item:first-child {
+            border-top-left-radius: 5px;
+            border-top-right-radius: 5px;
+          }
+  
+          .item:last-child {
+            border-bottom-right-radius: 5px;
+            border-bottom-left-radius: 5px;
+          }
+
+          .item.header {
+            font-weight: 600;
+            text-transform: uppercase;
+            text-align: center;
+          }
+
+          .item.clickable {
+            cursor: pointer;
+            font-weight: 600;
+            text-transform: uppercase;
+          }
+    
+          .item.clickable:hover {
+            background-color: #F1FCFF;
+          }
+    
+          .item.clickable:active {
+            background-color: #e0f6ff;
           }
         `}</style>
       </div>

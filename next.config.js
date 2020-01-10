aff474585bfdce0454c2ceef847f71c7fb0cb9c6
Kamil Wasilonek <kamilwasilonek1@gmail.com>
2020-01-10 14:19:22 +0100

    Add env variables configuration

--- next.config.js
+++ next.config.js
@@ -2,5 +2,16 @@ const withImages = require('next-images');
 const withWorkers = require('@zeit/next-workers');
 const withCSS = require('@zeit/next-css');
 const withOffline = require('next-offline');
+const { parsed: localEnv } = require('dotenv').config()
+const webpack = require('webpack')
 
-module.exports = withWorkers(withCSS(withImages(withOffline())));
+module.exports = withWorkers(
+  withCSS(
+    withImages({
+      webpack(config) {
+        config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
+        return config
+      }
+    })
+  )
+);
